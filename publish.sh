35d7688b6aea840c38f2ed57235225302ebce1de
Andre Loureiro <andreloureiroo@gmail.com>
2016-01-10 13:18:53 -0200

    Add CloudFront invalidation command

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
-aws s3 rm s3://andreloureiro.net/ --recursive
 aws s3 sync _site s3://andreloureiro.net --region sa-east-1
+aws cloudfront create-invalidation --distribution-id E26GPORVHMP7PF --paths /*
+
